9b72d5430a4debcc25041277fe0fdbb11574255d
Mohit Sharma <130201481+Mohitsholey04@users.noreply.github.com>
2024-02-13 18:07:58 +0530

    Create a User Database Using Postgres SQL Database.

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: ".local.env",
+        // envFilePath: ".prod.env",
+        
+      })],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        synchronize: true,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
